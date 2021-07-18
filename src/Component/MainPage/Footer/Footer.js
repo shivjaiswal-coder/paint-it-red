b85d8c818cbfc4f0bfd0187c1b5746ffceb86312
mayank18100 <mayank18100@iiitnr.edu.in>
2021-07-18 13:11:28 +0530

    Adding Footer

--- src/Component/MainPage/Footer/Footer.js
+++ src/Component/MainPage/Footer/Footer.js
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import './Footer.css'
 function Footer() {
     return (
         <div>
-            <p>this is from footer</p>
+            <div class="footer-dark">
+                <footer>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6 col-md-3 item">
+                                <h3>Services</h3>
+                                <ul>
+                                    <li><a href="#">Premium Paintings</a></li>
+                                    <li><a href="#">Wall Paintings</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-6 col-md-3 item">
+                                <h3>About</h3>
+                                <ul>
+                                    <li><a href="#">Company</a></li>
+                                    <li><a href="#">Team</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6 item text">
+                                <h3>Three Painters</h3>
+                                <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                            </div>
+                            <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                        </div>
+                        <p class="copyright">Three Painters © 2021</p>
+                    </div>
+                </footer>
+            </div>
         </div>
     )
 }
