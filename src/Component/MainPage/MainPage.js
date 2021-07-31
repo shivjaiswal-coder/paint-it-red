6cd77e9859961216d2fe15fa4f69e06fa7d64838
shivjaiswal-coder <shivjaiswal2000@gmail.com>
2021-07-31 11:38:26 +0530

    update

--- src/Component/MainPage/MainPage.js
+++ src/Component/MainPage/MainPage.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import Mainbar from "./Mainbar/Mainbar";
 import Navlinks from "./Navlinks/Navlinks";
-import Searchbar from "./Searchbar/Searchbar";
 import Header from "./Searchbar/Header";
 function MainPage() {
   return (
